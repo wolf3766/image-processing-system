070107ba28d6dd42957cd8fc71f178ec41e9c765
skc3766 <skc3766@gmail.com>
2024-09-01 13:10:33 +0530

    added api

--- dist/routes/webhook.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-router.post('/', (req, res) => {
-    console.log('Webhook received:', req.body);
-    res.status(200).send('Webhook processed');
-});
-exports.default = router;
